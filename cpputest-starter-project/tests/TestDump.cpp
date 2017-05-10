49bc46c54155398b46f113586e05df6c5a0477ab
Hank Barta <hbarta@gmail.com>
2017-05-10 15:26:34 -0500

    added tests for formatLine()

--- cpputest-starter-project/tests/TestDump.cpp
+++ cpputest-starter-project/tests/TestDump.cpp
@@ -11,32 +11,45 @@
 // #define BUF_SIZE    4096
 // static char buf[BUF_SIZE];
 
-extern "C"
-{
-	/*
+extern "C" {
+/*
 	 * You can add your c-only include files here
 	 */
-    #include <stdio.h>
-    #include "txtutil.h"
+#include <stdio.h>
+#include "txtutil.h"
 }
 
-TEST_GROUP(Dump)
+TEST_GROUP(Dump){
+    void setup(){}
+
+    void teardown(){}};
+
+TEST(Dump, test_endLine)
 {
-    void setup()
-    {
-    }
-
-    void teardown()
-    {
-    }
-};
-
-TEST(Dump, test1)
-{
-    /*
-     * Instantiate your class, or call the function, you want to test
-     */
-     STRCMP_EQUAL("                 |hello world|", endLine("hello world"))
-
+    // test endLine
+    STRCMP_EQUAL("                 |hello world|", endLine("hello world"))
+    STRCMP_EQUAL("                                             |hd|", endLine("hd"))
+    STRCMP_EQUAL("                                                   ||", endLine(""))
+    STRCMP_EQUAL("                           |hello wo|", endLine("hello wo"))
+    STRCMP_EQUAL("                       |hello wor|", endLine("hello wor"))
+    STRCMP_EQUAL("  |hello world agai|", endLine("hello world agai"))
+    STRCMP_EQUAL("  |hello world agai|", endLine("hello world again")) // pathological case
 }
+TEST(Dump, test_formatLine)
+{
+    // test formatLine
+    STRCMP_EQUAL("00000000  00 65 6c 6c 6f                                    |.ello|", formatLine(0, "\0ello", 5))
+    STRCMP_EQUAL("00000000  68 65 6c 6c 6f 20 77 6f  72 6c 64                 |hello world|", formatLine(0, "hello world", 11))
+    STRCMP_EQUAL("00000000  68 65 6c 6c 6f 20 77 6f  72 6c 64 20 61 67 61 69  |hello world agai|", formatLine(0, "hello world agai", 16))
+    STRCMP_EQUAL("0000000a                                                    ||", formatLine(10, "", 0))
 
+    // check all possible data values
+    char    testStr[] = "hello";
+    char    testOut[] = "00000000  00 65 6c 6c 6f                                    |.ello|";
+    for(int i=0; i<256; i++) {
+        testStr[0] = i;
+        sprintf(testOut, "00000000  %2.2x 65 6c 6c 6f                                    |%cello|", (unsigned int)(i & 0xFF), isprint(i)?i:'.');
+        fprintf(stderr, "%s\n", formatLine(0, testStr, 5)); // eyeball output
+        STRCMP_EQUAL(testOut, formatLine(0, testStr, 5));
+    }
+}

effdfff28d80f812a833a1d3072f63a2b4297a07
Hank Barta <hbarta@gmail.com>
2017-05-10 15:25:08 -0500

    code formatLine() and tweak endLine()

--- dump.c
+++ dump.c
@@ -13,22 +13,29 @@ hbarta@swanky:~/Documents/go-wks/src/github.com/HankB/playground$
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "txtutil.h"
 
-static FILE* fp = NULL;
+static FILE *fp = NULL;
 static int count = 16;
+#define COUNT 16
 
+#define LINE_BUF_LEN 100
 
-#define LINE_BUF_LEN        100
+// min() macro from http://stackoverflow.com/questions/3437404/min-and-max-in-c
+#define min(x, y) ( \
+    { __auto_type __x = (x); __auto_type __y = (y); \
+      __x < __y ? __x : __y; })
 
 /** @brief change output FILE *
  *
  * @param New FILE* to use for output
  * @retval previous FILE*
  */
-FILE* setStdout(FILE* newFp) {
-    FILE* prevFp = fp;
+FILE *setStdout(FILE *newFp)
+{
+    FILE *prevFp = fp;
     fp = newFp;
     return prevFp;
 }
@@ -44,28 +51,73 @@ FILE* setStdout(FILE* newFp) {
  * 
  * @note A static buffer is used to hold the returned string
  * which will be overwritten by subsequent calls.
+ * (TODO: Add buffer to parameter to save a memory copy)
  */
-const char * endLine(const char *str) {
-    int len = strlen(str);  // length of incoming string
+const char *endLine(const char *str)
+{
     static char line[LINE_BUF_LEN];
+    int len = strlen(str); // length of incoming string
+    int lenSoFar = 0;
 
     // adding spaces so ASCII representation lines up with previous lines
-    int lenSoFar = 0;
-    for(int i=len; i<count; i++) {
-        strncat(line+lenSoFar, "   ", LINE_BUF_LEN-lenSoFar);
-        lenSoFar += 3;  
+    line[0] = '\0'; // init buffer
+    for (int i = len; i < count; i++)
+    {
+        strncat(line + lenSoFar, "   ", LINE_BUF_LEN - lenSoFar);
+        lenSoFar += 3;
     }
 
-    if(len < count/2)  {    // need extra midline ' '?
-        strncat(line+lenSoFar, " ", LINE_BUF_LEN-lenSoFar);\
+    if (len <= count / 2)
+    { // need extra midline ' '?
+        strncat(line + lenSoFar, " ", LINE_BUF_LEN - lenSoFar);
         lenSoFar++;
     }
 
-    strncat(line+lenSoFar, "  |", LINE_BUF_LEN-lenSoFar);
-    lenSoFar += 3;  
-    strncat(line+lenSoFar, str, LINE_BUF_LEN-lenSoFar);
-    lenSoFar += len;  
-    strncat(line+lenSoFar, "|", LINE_BUF_LEN-lenSoFar);
-    
+    strncat(line + lenSoFar, "  |", LINE_BUF_LEN - lenSoFar);
+    lenSoFar += 2;
+    strncat(line + lenSoFar, str, min(LINE_BUF_LEN - lenSoFar, count));
+    lenSoFar += min(len, count);
+    strncat(line + lenSoFar, "|", LINE_BUF_LEN - lenSoFar);
+
+    return line;
+}
+
+/** @brief format a line of the 'hesdump' output
+ * @param index of the first byte in the data.
+ * @param pointer to the data
+ * @param number of bytes of valid data in data
+ * @retval formated line.
+ *
+ * @note data can contain null and non-printable values.
+ * 
+ */
+const char *formatLine(int byteIndex, const char *data, int dataLen)
+{
+    char asciiRep[COUNT + 1];
+    static char line[LINE_BUF_LEN];
+    int bytes = min(dataLen, count); // ignore surplus data
+    int stringLen;
+
+    asciiRep[0] = '\0'; // init the string
+    snprintf(line, LINE_BUF_LEN, "%8.8x ", byteIndex);
+
+    for (int i = 0; i < bytes; i++)
+    {
+        stringLen = strlen(line);
+        snprintf(line + stringLen, LINE_BUF_LEN - stringLen, " %2.2x", (unsigned int)(data[i] & 0xFF));
+        if (i == count / 2 - 1)
+        {
+            line[stringLen + 3] = ' ';
+            line[stringLen + 4] = '\0';
+        }
+        if (isprint(data[i])) // add character or '.'
+            asciiRep[i] = data[i];
+        else
+            asciiRep[i] = '.';
+        asciiRep[i + 1] = '\0'; // terminate string
+    }
+
+    strncat(line, endLine(asciiRep), LINE_BUF_LEN);
+
     return line;
 }

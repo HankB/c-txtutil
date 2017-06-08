26973629b914f16864c52ed878a5e34a12b1e1c3
Hank Barta <hbarta@gmail.com>
2017-06-07 19:49:08 -0500

    declare dump()

--- txtutil.h
+++ txtutil.h
@@ -7,4 +7,8 @@
 const char * endLine(const char *str);
 const char* formatLine(int byteIndex, const char* data, int dataLen);
 
+// public APIs
+FILE *setStdout(FILE *newFp);
+void dump(const char* data, size_t count);
+
 #endif // TXTUTIL_H

c072fd13d898bf3d4e889ef7bab480551bb7708b
Hank Barta <hbarta@gmail.com>
2017-06-12 21:05:00 -0500

    code format, additional tests

--- cpputest-starter-project/tests/TestDump.cpp
+++ cpputest-starter-project/tests/TestDump.cpp
@@ -20,7 +20,8 @@ TEST_GROUP(txtutil){
 
     void teardown(){}};
 
-TEST(txtutil, test_endLine){
+TEST(txtutil, test_endLine)
+{
     // test endLine
     STRCMP_EQUAL("                 |hello world|", endLine("hello world"));
     STRCMP_EQUAL("                                             |hd|", endLine("hd"));
@@ -29,7 +30,7 @@ TEST(txtutil, test_endLine){
     STRCMP_EQUAL("                       |hello wor|", endLine("hello wor"));
     STRCMP_EQUAL("  |hello world agai|", endLine("hello world agai"));
     STRCMP_EQUAL("  |hello world agai|", endLine("hello world again")); // pathological case
-} 
+}
 
 TEST(txtutil, test_formatLine)
 {
@@ -63,22 +64,31 @@ TEST_GROUP(dump){
 
     void setup(){
         fp = fopen(scratchFileName, "w+");
-if (fp == NULL)
-{
-    fprintf(stderr, "Cannot open \"%s\"\n", scratchFileName);
-    exit(1);
-}
-oldFp = setStdout(fp);
-}
+        if (fp == NULL)
+        {
+            fprintf(stderr, "Cannot open \"%s\"\n", scratchFileName);
+            exit(1);
+        }
+        oldFp = setStdout(fp);
+    }
 
-void teardown()
-{
-    setStdout(oldFp);
-    fclose(fp);
-}
-}
-;
+    void teardown()
+    {
+        setStdout(oldFp);
+        fclose(fp);
+    }
+};
 
+/**
+ * Testing strategy for dump().
+ * 1) Divert output to a disk file.
+ * 2) Retrieve contents using getOut() (to compare with a string.)
+ * 3) getOut() closes and renames file to save for examination
+ *    and to clear file contents for next operation.
+ *    getOut() opens new file for nest output operation.
+ * NOTE: file names will clash if more than one user runs this test
+ * at the same time!
+ */
 const char *getOut(void)
 {
     size_t bytesRead;
@@ -97,6 +107,7 @@ const char *getOut(void)
         fprintf(stderr, "Cannot open \"%s\"\n", scratchFileName);
         exit(1);
     }
+
     return buf;
 }
 
@@ -123,7 +134,31 @@ TEST(dump, test_dump)
     char buf[BUFLEN];
     for (int i = 0; i < BUFLEN; i++)
         buf[i] = i;
-        
+
     dump(buf, 16);
-    STRCMP_EQUAL("00000000  00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|\n00000010\n", getOut())
+    STRCMP_EQUAL(   "00000000  00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|\n"
+                    "00000010\n",
+                    getOut())
+    dump(buf, 17);
+    STRCMP_EQUAL(   "00000000  00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|\n"
+                    "00000010  10                                                |.|\n"
+                    "00000011\n", getOut())
+    dump(buf, 32);
+    STRCMP_EQUAL(   "00000000  00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|\n"
+                    "00000010  10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|\n"
+                    "00000020\n", getOut())
+    dump(buf, 128);
+    STRCMP_EQUAL(   "00000000  00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|\n"
+                    "00000010  10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|\n"
+                    "00000020  20 21 22 23 24 25 26 27  28 29 2a 2b 2c 2d 2e 2f  | !\"#$%&'()*+,-./|\n"
+                    "00000030  30 31 32 33 34 35 36 37  38 39 3a 3b 3c 3d 3e 3f  |0123456789:;<=>?|\n"
+                    "00000040  40 41 42 43 44 45 46 47  48 49 4a 4b 4c 4d 4e 4f  |@ABCDEFGHIJKLMNO|\n"
+                    "00000050  50 51 52 53 54 55 56 57  58 59 5a 5b 5c 5d 5e 5f  |PQRSTUVWXYZ[\\]^_|\n"
+                    "00000060  60 61 62 63 64 65 66 67  68 69 6a 6b 6c 6d 6e 6f  |`abcdefghijklmno|\n"
+                    "00000070  70 71 72 73 74 75 76 77  78 79 7a 7b 7c 7d 7e 7f  |pqrstuvwxyz{|}~.|\n"
+                    "00000080\n", getOut())
+    dump(buf, 15);
+    STRCMP_EQUAL(   "00000000  00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e     |...............|\n"
+                    "0000000f\n",
+                    getOut())
 }

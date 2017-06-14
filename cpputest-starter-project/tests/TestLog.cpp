9bf69776ab3aed427b8a5433ee06bc1b385bc5c7
Hank Barta <hbarta@gmail.com>
2017-06-13 22:07:34 -0500

    copy getOut() and implement add'l tests

--- cpputest-starter-project/tests/TestLog.cpp
+++ cpputest-starter-project/tests/TestLog.cpp
@@ -23,3 +23,74 @@ TEST(logutil, test_logPri)
     CHECK(setLogPri(4) == 3)
     CHECK(getLogPri() == 4)
 }
+
+// code for capturing output of the logmsg() API for testing purposes
+
+static FILE *fp;
+static FILE *oldFp;
+#define BUF_SIZE (1024 * 16)
+static char buf[BUF_SIZE];
+static const char *scratchFileName = "/tmp/TDD_log_test_file.txt";
+static const char *scratchPrevFileName = "/tmp/prevTDD_log_test_file.txt";
+
+/**
+ * Testing strategy for logmsg(). (cleverly copied from TestDump.cpp)
+ * 1) Divert output to a disk file.
+ * 2) Retrieve contents using getOut() (to compare with a string.)
+ * 3) getOut() closes and renames file to save for examination
+ *    and to clear file contents for next operation.
+ *    getOut() opens new file for nest output operation.
+ * NOTE: file names will clash if more than one user runs this test
+ * at the same time!
+ */
+static const char *getOut(void)
+{
+    size_t bytesRead;
+    fseek(fp, 0, SEEK_SET);                  // sek to start of file
+    bytesRead = fread(buf, 1, BUF_SIZE, fp); // read contents
+    buf[bytesRead] = '\0';                   // null terminate the input
+    fclose(fp);                              // close the file
+    if (rename(scratchFileName, scratchPrevFileName))
+    { // rename so we can examine contents (if necessary)
+        fprintf(stderr, "file rename error\n");
+        exit(1);
+    }
+    fp = fopen(scratchFileName, "w+"); // open the file again
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Cannot open \"%s\"\n", scratchFileName);
+        exit(1);
+    }
+
+    return buf;
+}
+
+
+TEST_GROUP(log){
+    void setup(){
+        fp = fopen(scratchFileName, "w+");
+        if (fp == NULL)
+        {
+            fprintf(stderr, "Cannot open \"%s\"\n", scratchFileName);
+            exit(1);
+        }
+        oldFp = setLogout(fp);
+    }
+
+    void teardown()
+    {
+        setLogout(oldFp);
+        fclose(fp);
+    }
+};
+
+TEST(log, test_logOut)
+{
+    CHECK(oldFp == stdout)
+    CHECK(logmsg(0, "Hello World") == 0)
+    CHECK(logmsg(3, "Hello World") == 11)
+    STRCMP_EQUAL("Hello World", getOut())
+    CHECK(logmsg(3, "%s", "Goodbye World") == 13)
+    STRCMP_EQUAL("Goodbye World", getOut())
+
+}
